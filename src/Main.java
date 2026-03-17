1eb4584b935c5388708b158984a67921646c5a78
divijap01 <divijavikaspawar01@gmail.com>
2026-03-17 22:58:42 +0530

    UC3: Centralized room inventory management

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,31 @@
-// Abstract Class
-abstract class Room {
-    private int beds;
-    private int size;
-    private double price;
+import java.util.HashMap;
 
-    // Constructor
-    public Room(int beds, int size, double price) {
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
+/**
+ * RoomInventory - Centralized room inventory management system
+ * Maintains single source of truth for room availability
+ */
+public class RoomInventory {
+    private HashMap<String, Integer> inventory;
+
+    public RoomInventory() {
+        inventory = new HashMap<>();
+        inventory.put("Single Room", 10);
+        inventory.put("Double Room", 8);
+        inventory.put("Suite Room", 5);
     }
 
-    // Getters
-    public int getBeds() {
-        return beds;
+    public int getAvailability(String roomType) {
+        return inventory.getOrDefault(roomType, 0);
     }
 
-    public int getSize() {
-        return size;
+    public void updateAvailability(String roomType, int count) {
+        inventory.put(roomType, count);
     }
 
-    public double getPrice() {
-        return price;
-    }
-
-    // Method to display room details
-    public void displayRoomDetails() {
-        System.out.println("Beds: " + beds);
-        System.out.println("Room Size: " + size + " sq ft");
-        System.out.println("Price: ₹" + price);
-    }
-}
-
-// Single Room Class
-class SingleRoom extends Room {
-
-    public SingleRoom() {
-        super(1, 200, 2500);
-    }
-
-    public void roomType() {
-        System.out.println("Room Type: Single Room");
-    }
-}
-
-// Double Room Class
-class DoubleRoom extends Room {
-
-    public DoubleRoom() {
-        super(2, 350, 4000);
-    }
-
-    public void roomType() {
-        System.out.println("Room Type: Double Room");
-    }
-}
-
-// Suite Room Class
-class SuiteRoom extends Room {
-
-    public SuiteRoom() {
-        super(3, 600, 8000);
-    }
-
-    public void roomType() {
-        System.out.println("Room Type: Suite Room");
-    }
-}
-
-// Main Class
-public class UseCase2RoomInitialization {
-
-    public static void main(String[] args) {
-
-        // Static availability
-        int singleAvailable = 5;
-        int doubleAvailable = 3;
-        int suiteAvailable = 2;
-
-        // Creating objects using polymorphism
-        Room r1 = new SingleRoom();
-        Room r2 = new DoubleRoom();
-        Room r3 = new SuiteRoom();
-
-        System.out.println("------ Available Rooms ------\n");
-
-        System.out.println("Single Room");
-        r1.displayRoomDetails();
-        System.out.println("Available: " + singleAvailable);
-
-        System.out.println("\nDouble Room");
-        r2.displayRoomDetails();
-        System.out.println("Available: " + doubleAvailable);
-
-        System.out.println("\nSuite Room");
-        r3.displayRoomDetails();
-        System.out.println("Available: " + suiteAvailable);
+    public void displayInventory() {
+        System.out.println("=== Room Inventory ===");
+        for (String roomType : inventory.keySet()) {
+            System.out.println(roomType + ": " + inventory.get(roomType) + " available");
+        }
     }
 }
