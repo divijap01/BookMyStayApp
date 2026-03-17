d592fd2633a9e1f6dd3dd697e8329f1c7919e48a
divijap01 <divijavikaspawar01@gmail.com>
2026-03-17 23:18:11 +0530

    UC7: Add on service selection

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,18 @@
-import java.util.*;
+public class AddOnService {
 
-public class RoomAllocationService {
+    private String serviceName;
+    private double cost;
 
-    private Set<String> allocatedRoomIds;
-    private Map<String, Set<String>> assignedRoomsByType;
-
-    public RoomAllocationService() {
-        allocatedRoomIds = new HashSet<>();
-        assignedRoomsByType = new HashMap<>();
+    public AddOnService(String serviceName, double cost) {
+        this.serviceName = serviceName;
+        this.cost = cost;
     }
 
-    public void allocateRoom(Reservation reservation, RoomInventory inventory) {
-        String type = reservation.getRoomType();
-
-        if (inventory.isAvailable(type)) {
-            String roomId = generateRoomId(type);
-
-            allocatedRoomIds.add(roomId);
-
-            assignedRoomsByType.putIfAbsent(type, new HashSet<>());
-            assignedRoomsByType.get(type).add(roomId);
-
-            inventory.decrementInventory(type);
-            reservation.confirm(roomId);
-        }
+    public String getServiceName() {
+        return serviceName;
     }
 
-    private String generateRoomId(String roomType) {
-        String newId;
-        do {
-            newId = roomType.toUpperCase().substring(0, 3) + "-" + (int)(Math.random() * 1000);
-        } while (allocatedRoomIds.contains(newId));
-        return newId;
+    public double getCost() {
+        return cost;
     }
 }
